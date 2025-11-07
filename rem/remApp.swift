c87d9abaed6aa8ccad3d9e5eeb37c10a7ad23359
artem <itnikitins@gmail.com>
2025-11-07 23:00:11 +0300

    initial commit

---
+++ rem/remApp.swift
@@ -0,0 +1,17 @@
+//
+//  remApp.swift
+//  rem
+//
+//  Created by Никитин Артем on 4.11.25.
+//
+
+import SwiftUI
+
+@main
+struct remApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
